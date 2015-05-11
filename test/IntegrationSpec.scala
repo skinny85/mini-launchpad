b3fdda7ce106cdd04285a19008fe6cf3eed8a1c2
Adam Ruka <adamruka85@gmail.com>
2015-05-12 00:22:55 +0200

    Update tests.

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -5,16 +5,9 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 @RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
-
+class IntegrationSpec extends Specification {
   "Application" should {
-
     "send 404 on a bad request" in new WithApplication{
       route(FakeRequest(GET, "/boum")) must beNone
     }
@@ -24,7 +17,7 @@ class ApplicationSpec extends Specification {
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      contentAsString(home) must contain ("Wordpress")
     }
   }
 }
